dda7ba2b5bfd8bff10e76afe715d154348411787
Bjorn-Christensen <bjorn.christensen@mail.mcgill.ca>
2025-04-15 16:06:21 -0400

    Small optimizations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -45,7 +45,7 @@ public final class Constants {
 
     // Drivetrain Constants
     public static final class DrivetrainConstants {
-        public static final double MAX_SPEED = Units.feetToMeters(15.0);
+        public static final double MAX_SPEED = Units.feetToMeters(15.5); // Theoretically: ~19.2-19.8, Real: 15-17
         public static final double WHEEL_LOCK_TIME = 10; // Hold time on motor brakes when disabled (seconds)
     }
 
